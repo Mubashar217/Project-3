b19d5c5576bad13b824640ee2fa24c84deb63f69
Mubashar217 <muhammadmubasharoffical@gmail.com>
2025-04-09 12:16:23 +0500

    dell

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,4 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-}
-
-<<<<<<< HEAD
-module.exports = nextConfig;
-=======
-module.exports = nextConfig;
->>>>>>> 1800265875e6e8aad92b2eff37caf81a98fbb91d
+};
