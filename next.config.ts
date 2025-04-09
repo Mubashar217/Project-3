e41e16fd00856f052820fe3377294a9ee8c3876a
Muhammad Mubasahar <muhammadmubasharoffical@gmail.com>
2025-04-09 11:56:32 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
-/** @type {import('next').NextConfig} */
+@type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // آپ کی کنفیگریشن یہاں
 }
 
-<<<<<<< HEAD
-module.exports = nextConfig;
-=======
-module.exports = nextConfig;
->>>>>>> 1800265875e6e8aad92b2eff37caf81a98fbb91d
+module.exports = nextConfig
