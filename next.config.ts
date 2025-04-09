8723d0642e7343fd0df8788c9a4b9984d6c68598
Muhammad Mubasahar <muhammadmubasharoffical@gmail.com>
2025-04-09 12:22:00 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-<<<<<<< HEAD
-};
-=======
-  webpack: (config) => {
-    // اپنی تخصیصات یہاں شامل کریں
-    return config;
-  },
 }
 
 module.exports = nextConfig;
->>>>>>> 1712e57e2e4070e7c2b3b98384a5f390658f2159
